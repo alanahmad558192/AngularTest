457657b2145605c11d32f1effd2424574e8ddcac
Alan Ahmad <aahmad@sevenseasworldwide.com>
2024-03-25 15:51:35 +0800

    Create SpyObject

--- Test/unit-testing/src/app/calc.service.spec.ts
+++ Test/unit-testing/src/app/calc.service.spec.ts
@@ -1,9 +1,18 @@
 import { CalcService } from "./calc.service";
+import { SharedService } from "./shared.service";
 
 describe("CalcService", () => {
-  it("should multiply two numbers", ()=> {
-    const calc = new CalcService();
+  it("should multiply two numbers", () => {
+    const shared = new SharedService();
+    const calc = new CalcService(shared);
     const result = calc.multiply(3,5);
     expect(result).toBe(15);
   });
+
+  it("should call the mySharedFunction func", () => {
+    const shared = jasmine.createSpyObj("SharedService", ["mySharedFunction"]);
+    const calc = new CalcService(shared);
+    const result = calc.multiply(3,5);
+    expect(result).toBe(15);
+  })
 })
